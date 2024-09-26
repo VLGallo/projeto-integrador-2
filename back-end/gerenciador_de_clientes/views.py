cbff71828a511e949e0ddb2efa4e6f07a1dffbb2
Vanessa Leticia Gallo <96696812+VLGallo@users.noreply.github.com>
2024-09-25 22:39:43 -0300

    altera a classe ClienteUpdateView
    atualiza essa classe para conter os novos campos extraídos da api de cep

--- back-end/gerenciador_de_clientes/views.py
+++ back-end/gerenciador_de_clientes/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 import requests
 from requests import Response
 from rest_framework.exceptions import NotFound
@@ -70,10 +71,31 @@ class ClienteUpdateView(APIView):
 
     def put(self, request, pk):
         cliente = self.get_object(pk)
-        serializer = ClienteSerializer(cliente, data=request.data)
+        cep = request.data.get('cep')
+        if not cep:
+            return Response({"error": "CEP é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+
+        response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+        if response.status_code != 200 or 'erro' in response.json():
+            return Response({"error": "CEP inválido."}, status=status.HTTP_400_BAD_REQUEST)
+
+        endereco_data = response.json()
+        logradouro = endereco_data.get('logradouro', '')
+        bairro = endereco_data.get('bairro', '')
+
+        serializer = ClienteSerializer(cliente, data={
+            'nome': request.data.get('nome'),
+            'telefone': request.data.get('telefone'),
+            'cep': cep,
+            'logradouro': logradouro,
+            'numero': request.data.get('numero'),
+            'complemento': request.data.get('complemento', ''),
+            'bairro': bairro
+        })
+
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ClienteDeleteView(APIView):
